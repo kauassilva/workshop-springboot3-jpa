21a58887c70f16efe236f653320efefe434006a9
Kaua Silva <kauassilva1@gmail.com>
2023-11-02 20:56:30 -0300

    Added delete method to Category

--- src/main/java/com/kauadss/course/services/CategoryService.java
+++ src/main/java/com/kauadss/course/services/CategoryService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.kauadss.course.entities.Category;
 import com.kauadss.course.repositories.CategoryRepository;
+import com.kauadss.course.services.exceptions.DatabaseException;
 import com.kauadss.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -27,6 +29,17 @@ public class CategoryService {
 	
 	public Category insert(Category obj) {
 		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+		}
+		try {
+			repository.deleteById(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 }
