4e7e87ace7ca33a901335bfed174e1fc0ae75471
Kaua Silva <kauassilva1@gmail.com>
2023-11-02 19:22:15 -0300

    Removed unnecessary annotation in repository files

--- src/main/java/com/kauadss/course/repositories/OrderRepository.java
+++ src/main/java/com/kauadss/course/repositories/OrderRepository.java
@@ -1,10 +1,8 @@
 package com.kauadss.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.kauadss.course.entities.Order;
 
-@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
--- src/main/java/com/kauadss/course/repositories/UserRepository.java
+++ src/main/java/com/kauadss/course/repositories/UserRepository.java
@@ -1,10 +1,8 @@
 package com.kauadss.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.kauadss.course.entities.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
