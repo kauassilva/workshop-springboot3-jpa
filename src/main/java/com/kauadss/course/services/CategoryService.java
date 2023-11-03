2fefd37e31aa1786e1cdba0617519ded12aa2012
Kaua Silva <kauassilva1@gmail.com>
2023-11-03 11:45:10 -0300

    Added delete method to Category

--- src/main/java/com/kauadss/course/services/CategoryService.java
+++ src/main/java/com/kauadss/course/services/CategoryService.java
@@ -12,6 +12,8 @@ import com.kauadss.course.repositories.CategoryRepository;
 import com.kauadss.course.services.exceptions.DatabaseException;
 import com.kauadss.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class CategoryService {
 	
@@ -40,6 +42,20 @@ public class CategoryService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
+	}
+	
+	public Category update(Long id, Category obj) {
+		try {			
+			Category entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
 	}
 
 }
